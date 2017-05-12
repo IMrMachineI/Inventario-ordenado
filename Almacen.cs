6e438bdfd19137f4b76c6518aa07a5a1c8a059c7
IMrMachineI <fubu96meex@gmail.com>
2017-05-11 19:45:57 -0500

    Add files via upload

---
+++ Almacen.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventarioConListasOrdenado
+{
+    class Almacen
+    {
+        private Productos inicio;
+
+        public Almacen()
+        {
+            inicio = null;
+        }
+
+        public bool agregarProducto(Productos producto)
+        {
+            bool sePuedeAgregar = true;
+            if(inicio == null)
+            {
+                inicio = producto;
+            }
+            else
+            {
+                Productos varTemporal = inicio;
+                while(varTemporal.siguiente != null && sePuedeAgregar == true)
+                {
+                    varTemporal = varTemporal.siguiente;
+                }
+                if(varTemporal.codigo != producto.codigo && sePuedeAgregar)
+                {
+                    varTemporal = inicio;
+                    if (producto.codigo < varTemporal.codigo)
+                    {
+                        producto.siguiente = varTemporal;
+                        inicio = producto;
+                    }
+                    else
+                    {
+                        sePuedeAgregar = true;
+                        while(varTemporal.siguiente != null)
+                        {
+                            if(producto.codigo < varTemporal.codigo)
+                            {
+                                producto.siguiente = varTemporal.siguiente;
+                                varTemporal.siguiente = producto;
+                                sePuedeAgregar = false;
+                                break;
+                            }
+                        }
+                        if(sePuedeAgregar)
+                        {
+                            varTemporal.siguiente = producto;
+                        }
+                    }
+                }
+            }
+            return sePuedeAgregar;
+        }
+        public void agregar(Productos producto)
+        {
+            Productos varTemporal = inicio;
+            while(varTemporal.siguiente != null)
+            {
+                if(varTemporal.codigo < producto.codigo)
+                {
+                    producto.siguiente = varTemporal.siguiente;
+                    varTemporal.siguiente = producto;
+                }
+            }
+        }
+        public Productos buscarProducto(int codigo)
+        {
+            Productos varTemporal = inicio;
+            while(varTemporal != null)
+            {
+                varTemporal = varTemporal.siguiente;
+            }
+            return varTemporal;
+        }
+
+        public string reporte()
+        {
+            string resultado = "";
+            Productos varTemporal = inicio;
+            while(varTemporal != null)
+            {
+                resultado += varTemporal.ToString();
+                varTemporal = varTemporal.siguiente;
+            }
+            return resultado;
+        }
+        public void eliminarProducto(int codigo)
+        {
+            Productos varTemporal = inicio;
+            bool sePuedeEliminar = true;
+            if(varTemporal != null)
+            {
+                if(varTemporal.codigo == codigo)
+                {
+                    inicio = varTemporal.siguiente;
+                    varTemporal = inicio;
+                }
+                else
+                {
+                    while(varTemporal.siguiente != null)
+                    {
+                        varTemporal = varTemporal.siguiente;
+                    }
+                }
+            }
+        }
+        public void insertarProducto(int posicion, Productos producto)
+        {
+            int contador = 2;
+            Productos varTemporal = inicio;
+            if(posicion == 1)
+            {
+                producto.siguiente = varTemporal;
+                inicio = producto;
+            }
+            else
+            {
+                while(varTemporal.siguiente != null && contador != posicion)
+                {
+                    contador++;
+                    varTemporal = varTemporal.siguiente;
+                }
+                producto.siguiente = varTemporal.siguiente;
+                varTemporal.siguiente = producto;
+            }
+        }
+        public override string ToString()
+        {
+            string resultado = "";
+            Productos varTemporal = inicio;
+            while (varTemporal != null)
+            {
+                resultado += varTemporal.ToString();
+                varTemporal = varTemporal.siguiente;
+            }
+            return resultado;
+        }
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventarioConListasOrdenado
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Almacen nuevoAlmacen = new Almacen();
+        Productos nuevoProducto;
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+
+            nuevoProducto = new Productos(Convert.ToInt32(txtCodigo.Text), txtNombre.Text,
+                Convert.ToInt32(txtCantidad.Text), Convert.ToInt32(txtCosto.Text));
+            nuevoAlmacen.agregarProducto(nuevoProducto);
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (nuevoAlmacen.buscarProducto(Convert.ToInt32(txtCodigo.Text)) != null)
+            {
+                txtReporte.Text = (nuevoAlmacen.buscarProducto(Convert.ToInt32(txtCodigo.Text))).ToString();
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            nuevoAlmacen.eliminarProducto(Convert.ToInt32(txtCodigo.Text));
+            txtReporte.Text = nuevoAlmacen.reporte();
+        }
+
+        private void btnReporte_Click(object sender, EventArgs e)
+        {
+            txtReporte.Text = nuevoAlmacen.ToString();
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            nuevoProducto = new Productos(Convert.ToInt32(txtCodigo.Text), txtNombre.Text,
+                Convert.ToInt32(txtCantidad.Text), Convert.ToInt32(txtCosto.Text));
+            nuevoAlmacen.insertarProducto(Convert.ToInt32(txtInsertar.Text),nuevoProducto);
+        }
+    }
+}
